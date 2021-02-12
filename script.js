fa703bffd65ded76957efb5fe10725f4cf671024
Zinamarie <zmarie2303@gmail.com>
2021-02-12 18:23:30 -0500

    Updated winner!

--- script.js
+++ script.js
@@ -15,24 +15,32 @@ var createPoliticians = function(name, partyColor) {
 return politician;
 };
 
-//function for set state results
-  var setStateResults = function(state) {
-    theStates[state].winner = null;
 
-    if (patrick.results[state] > david.results[state]) {
-      theStates[state].winner = patrick; 
-    } else if (patrick.results[state] < david.results[state]) {
-      theStates[state].winner = david;
-    }
 
-    var stateWinner = theStates[state].winner;
-    if (stateWinner !== null) {
-      theStates[state].rgbColor = stateWinner.partyColor;
-    } else {
-      theStates[state].rgbColor = [255, 255, 255];
-    }
+ var patrick = createPoliticians('Patrick', [44, 60, 100]);
+ patrick.results = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4,8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
+ 
+ var david = createPoliticians('David', [24, 27, 31]);
+ david.results = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
 
-    // Declaring it all
+ //function for set state results
+ var setStateResults = function(state) {
+  theStates[state].winner = null;
+
+  if (patrick.results[state] > david.results[state]) {
+    theStates[state].winner = patrick; 
+  } else if (patrick.results[state] < david.results[state]) {
+    theStates[state].winner = david;
+  }
+
+  var stateWinner = theStates[state].winner;
+  if (stateWinner !== null) {
+    theStates[state].rgbColor = stateWinner.partyColor;
+  } else {
+    theStates[state].rgbColor = [255, 255, 255];
+  }
+
+  // Declaring it all
 stateName.innerText = theStates[state].nameFull;
 stateAbbr.innerText = "(" +theStates[state].nameAbbrev + ")";
 
@@ -43,28 +51,23 @@ candidateStateResults.innerText = patrick.results[state];
 candidateStateResults2.innerText = david.results[state];
 
 if (theStates[state].winner === null) {
-  candidateWinner.innerText = 'Draw'; 
+candidateWinner.innerText = 'Draw'; 
 } else {
-  candidateWinner.innerText = theStates[state].winner.name;
+candidateWinner.innerText = theStates[state].winner.name;
 }
 
 
-  };
-
- var patrick = createPoliticians('Patrick', [44, 60, 100]);
- patrick.results = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4,8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
- 
- var david = createPoliticians('David', [24, 27, 31]);
- david.results = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
-
+};
  
 //declaring the winner
-var winner = {}; 
+patrick.talliedUpVotes();
+david.talliedUpVotes();
+var winner = ''; 
 if (patrick.totalVotes > david.totalVotes) {
-winner = patrick.name;
+  winner = patrick.name;
 }
 else if (patrick.totalVotes < david.totalVotes) {
-         winner = david.name;
+  winner = david.name;
 } else {
 winner = 'It\'s a draw!';
 }
@@ -80,12 +83,6 @@ patrick.results[4] = 17;
 david.results[4] = 38;
 patrick.results[43] = 11;
 david.results[43] = 27;
-
-// tallied results
-console.log(patrick.results);
-console.log(david.results); 
-patrick.talliedUpVotes(); console.log(patrick.totalVotes);
-david.talliedUpVotes(); console.log(david.totalVotes);
 
 // Announcing winners
 var countryResults = document.getElementById('countryResults');
